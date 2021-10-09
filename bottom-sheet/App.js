fc778929ecaafd9a3e67b3a31b59b1d60d065225
macbook air <tuhinsaud4614@gmail.com>
2021-10-09 18:50:49 +0600

    Create Bottom-Sheet Component

--- bottom-sheet/App.js
+++ bottom-sheet/App.js
@@ -1,21 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { Button, ScrollView, StyleSheet, Text, View } from "react-native";
+import { Provider } from "react-native-paper";
+import BottomSheet from "./bottom-sheet/BottomSheet";
 
 export default function App() {
+  const [show, setShow] = useState(true);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider>
+      <View style={styles.container}>
+        <Button onPress={() => setShow(true)} title="Show Bottom Sheet" />
+        <BottomSheet
+          show={show}
+          onDismiss={() => {
+            setShow(false);
+          }}
+          enableBackdropDismiss
+        >
+          <ScrollView contentContainerStyle={{ padding: 16 }}>
+            {Array.from({ length: 40 }).map((_, index) => (
+              <Text key={index}>Text - {index + 1}</Text>
+            ))}
+          </ScrollView>
+        </BottomSheet>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ccc",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
